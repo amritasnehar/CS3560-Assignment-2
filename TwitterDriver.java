e0439d2bd64577cde2659a25fb97bb0f5b03640c
amritasnehar <112672313+amritasnehar@users.noreply.github.com>
2023-12-12 20:01:24 -0800

    Update TwitterDriver.java

--- TwitterDriver.java
+++ TwitterDriver.java
@@ -1,17 +1,5 @@
-import javax.swing.*;
-
-// Driver class (main method)
 public class TwitterDriver {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> extracted());
-    }
-
-    private static void extracted() {
-        AdminControlPanel adminControlPanel = new AdminControlPanel();
-        adminControlPanel.setVisible(true);
-        // For testing purposes, let's add a user and show UserView
-        adminControlPanel.addUserOrGroup("JohnDoe");
-        UserView userView = new UserView();
-        userView.setVisible(true);
+        // Your main logic for initiating the Twitter application
     }
 }
