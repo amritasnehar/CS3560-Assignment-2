99406820bdba3b8951e783780e7a0e6b4fe2ad29
amritasnehar <112672313+amritasnehar@users.noreply.github.com>
2023-12-12 20:01:06 -0800

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -1,7 +1,5 @@
 public interface Subject {
-    void addObserver(Observer observer);
-
+    void registerObserver(Observer observer);
     void removeObserver(Observer observer);
-
     void notifyObservers(String message);
 }
